fe66e3fa0aa743946536874c2013d35a64e41d2c
sandeepkunichi <sandeep.k@betsol.com>
2016-07-10 23:12:00 +0530

    Fixed CSRF issue. Returning request object

--- portal/views.py
+++ portal/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render_to_response
+from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.template import loader
 
@@ -9,23 +9,18 @@ from .forms import ReferralForm
 
 @login_required(login_url='/portal/login/')
 def index(request):
-    latest_question_list = JobPosting.objects.order_by('-pub_date')[:5]
-    context = {
-        'latest_question_list': latest_question_list,
-    }
     if request.method == 'POST':
         form = ReferralForm(request.POST)
         if form.is_valid():
             referral = Referral(referral_name=form.cleaned_data['referral_name'],
                                 referral_email=form.cleaned_data['referral_email'],
-                                job_posting=form.cleaned_data['job_postings'],
-                                status='WAITING')
+                                job_posting=form.cleaned_data['job_postings'])
 
             referral.save()
             return HttpResponseRedirect('/portal/')
     else:
         form = ReferralForm()
-    return render_to_response('portal/index.html', context=context, dictionary={'referral_form': form})
+    return render(request, 'portal/index.html', dictionary={'referral_form': form})
 
 
 @login_required(login_url='portal/login/')
